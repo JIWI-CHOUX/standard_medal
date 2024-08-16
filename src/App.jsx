93fd410c78cdb38a4224b3817e2177c2609fa166
JIWI-CHOUX <als.dn960418@gmail.com>
2024-08-16 16:12:59 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-
   const [country, setCountry] = useState("");
   const [gold, setGold] = useState(0);
   const [silver, setSilver] = useState(0);
@@ -13,10 +12,10 @@ const App = () => {
       if (eventCountry.name.toLowerCase() === country.toLowerCase()) {
         return true;
       } else {
-        return false; 
+        return false;
       }
     });
-  }
+  };
 
   const addCountry = (event) => {
     event.preventDefault();
@@ -30,28 +29,27 @@ const App = () => {
         silver: silver,
         bronze: bronze,
       };
-      setCountries([...countries, newCountry]) ;
-    };
+      setCountries([...countries, newCountry].sort((a, b) => b.gold - a.gold));
     }
+  };
 
-  
   const updateCountries = (event) => {
     event.preventDefault();
 
-    if (checkExistingCountry()) {
+    if(checkExistingCountry) {
       setCountries(
         countries.map((eventCountry) => {
           if(eventCountry.name.toLowerCase() === country.toLowerCase()) {
             return {
-              name: country.name,
+              name: country,
               gold: gold,
               silver: silver,
               bronze: bronze,
             }
           } else {
-            return country;
+            return eventCountry;
           }
-        })
+        }).sort((a, b) => b.gold - a.gold)
       )
     } else {
       alert("등록되지 않은 국가입니다.");
@@ -59,83 +57,83 @@ const App = () => {
   }
 
   const deleteCountry = (name) => {
-    console.log(name);
     setCountries(countries.filter((country) => {
       if(country.name.toLowerCase() !== name) {
         return country;
-      };
+      }
     }))
   }
 
+
   return (
-    <div className="main-container">
+    <div>
       <h1>2024 파리 올림픽 메달 리스트</h1>
-      <form className="input-form" onSubmit={addCountry}>
-        <div className="input-field">
-          <label htmlFor="country">국가명</label>
+      <form onSubmit={addCountry}>
+        <div>
+          <label htmlFor="country">국가 이름</label>
           <input
             id="country"
             onChange={(event) => {
               setCountry(event.target.value);
             }}
-          />
+          ></input>
         </div>
-        <div className="input-field">
+        <div>
           <label htmlFor="gold">금메달</label>
           <input
-            id="gole"
+            id="gold"
             onChange={(event) => {
               setGold(event.target.value);
             }}
-          />
+          ></input>
         </div>
-        <div className="input-field">
+        <div>
           <label htmlFor="silver">은메달</label>
           <input
             id="silver"
             onChange={(event) => {
               setSilver(event.target.value);
             }}
-          />
+          ></input>
         </div>
-        <div className="input-field">
+        <div>
           <label htmlFor="bronze">동메달</label>
           <input
             id="bronze"
             onChange={(event) => {
               setBronze(event.target.value);
             }}
-          />
-          <div className="button-group">
-            <button type="submit">국가추가</button>
+          ></input>
+          <div>
+            <button type="submit">국가 추가</button>
             <button onClick={updateCountries}>수정하기</button>
           </div>
         </div>
       </form>
-      <div className="table-wrapper">
+      <div>
         <table>
           <thead>
             <tr>
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
+              <th>국가 이름</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>기능</th>
             </tr>
           </thead>
           <tbody>
             {countries.map((country) => {
               return (
-                <tr key={country.name}>
+                <tr key = {country.name}>
                   <td>{country.name}</td>
                   <td>{country.gold}</td>
                   <td>{country.silver}</td>
                   <td>{country.bronze}</td>
-                  <td className="delete-btn" 
+                  <td
                   onClick={() => {
-                    deleteCountry(country.name);
-                  }}>삭제</td>
+                    deleteCountry(country.name)}}>삭제</td>
                 </tr>
-              );
+              )
             })}
           </tbody>
         </table>
